17f2c729014844920fffefd14215363fae5ef26c
Pablo Cortés Rodríguez <cortesrodriguezpablo3@gmail.com>
2023-12-24 14:55:30 +0100

    Version 0.9 - Setting up environment variables to match the server

--- src/enviroment.js
+++ src/enviroment.js
@@ -1,3 +1,3 @@
 
-export const API_URL = "http://192.168.92.96:5000" // "http://18.101.98.119" 
+export const API_URL = "http://18.101.98.119/" // "http://18.101.98.119" 
 export const DBNAME = "tickets.db"
