6b8cf8a275e2d890febfc228b9b137437576db59
Pablo Cortés Rodríguez <cortesrodriguezpablo3@gmail.com>
2024-03-04 11:37:11 +0100

    Setting new url for api

--- src/enviroment.js
+++ src/enviroment.js
@@ -1,3 +1,3 @@
 
-export const API_URL = "http://18.101.98.119/" // "http://18.101.98.119" 
+export const API_URL = "https://singiliabarba.informaticaantequera.com/" // "http://18.101.98.119" 
 export const DBNAME = "tickets.db"
